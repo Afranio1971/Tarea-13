320057793aa58abb5ce923e3ebb359657d9b1b3a
Afranio Santiago Condoy Correa <condoysantiago3@gmail.com>
2026-09-10 20:25:07 -0500

    Add files via upload

---
+++ Tarea 13/totalcompras.py
@@ -0,0 +1,12 @@
+def calcularTotal(precio, cantidad):
+    total = precio * cantidad
+    return total
+if __name__ == "__main__":
+    # Uso de la funcion
+    precio = 10
+    cantidad = 3
+
+    resultado = calcularTotal(precio, cantidad)
+
+    print(resultado)
+
